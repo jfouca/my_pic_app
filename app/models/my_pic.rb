9580dc74a029a1cd03577e2f8a67ecc05ed07979
jfoucault <jeremy.foucault@blablacar.com>
2018-02-24 23:31:10 +0100

    [master] add validation

--- app/models/my_pic.rb
+++ app/models/my_pic.rb
@@ -1,4 +1,10 @@
 class MyPic < ApplicationRecord
     # validations
+    FORMAT_OPTIONS = %w(jpg png)
+
+    validates :format, :inclusion => {:in => FORMAT_OPTIONS}
+    validates :width, numericality: { greater_than_or_equal_to: 1,  only_integer: true }
+    validates :height, numericality: { greater_than_or_equal_to: 1,  only_integer: true }
+
     validates_presence_of :text, :format, :width, :height
 end
