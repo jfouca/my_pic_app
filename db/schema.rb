9b324ed2b551ac884898cfd178541425387839a5
jfoucault <jeremy.foucault@blablacar.com>
2018-02-25 02:58:29 +0100

    [master] remove migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224205147) do
+ActiveRecord::Schema.define(version: 20180224191829) do
 
   create_table "my_pics", force: :cascade do |t|
     t.string "text"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20180224205147) do
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "url"
   end
 
 end
